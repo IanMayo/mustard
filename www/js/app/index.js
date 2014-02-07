b61d4b85738008553c155ccbc044d72b32c25f68
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-02-07 23:09:53 +0200

    Remove status bar on the main screen (expected behaviour for the games)

--- www/js/app/index.js
+++ www/js/app/index.js
@@ -33,6 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        StatusBar.hide();
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
