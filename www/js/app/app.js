80d97afff604f2bd3c86c783c1e915888d3e15e8
Evgeny Demidchenko <promocoding@gmail.com>
2014-02-12 12:14:12 +0200

    Add libs to game

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -1,11 +1,23 @@
-angular.module('mustard', ['ngRoute', 'mustard.services', 'mustard.game.simulator'])
+angular.module('mustard', ['ngRoute', 'mustard.services', 'mustard.game.simulator', 'leaflet-directive'])
 
 .config(function ($routeProvider) {
 
     $routeProvider
     .when('/game/mission', {
         controller: 'SimulatorCtrl',
-        templateUrl: 'js/game/controllers/simulator/mission.tpl.html'
+        templateUrl: 'js/game/controllers/simulator/mission.tpl.html',
+        resolve: {
+            scenario: ['$http', '$q', function ($http, $q) {
+                var deferred = $q.defer();
+                $http({
+                    method: "GET",
+                    url: "/www/js/game/scenarios/PracticeScenario.json"
+                }).success(function (response) {
+                        deferred.resolve(response);
+                    });
+                return deferred.promise;
+            }]
+        }
     })
     .when('/game/training', {
         controller: 'SimulatorCtrl',
