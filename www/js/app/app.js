0453756e0219034f5ec9a3e7ff293ccb6f7a57c5
Evgeny Demidchenko <promocoding@gmail.com>
2014-02-25 10:44:29 +0200

    Roll back paths config

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -13,11 +13,13 @@ angular.module('mustard', [
         resolve: {
             scenario: ['$http', '$q', '$route', function ($http, $q, $route) {
                 var deferred = $q.defer();
-                var scenario = $route.current.params.scenario.replace(/^\/?/,'') + '.json';
+                var scenario = $route.current.params.scenario + '.json';
+                console.log('scenario', scenario);
+
 
                 $http({
                     method: "GET",
-                    url: '/mustard/www/js/game/scenarios/' + scenario
+                    url: 'js/game/scenarios/' + scenario
                 }).success(function (response) {
                     deferred.resolve(response);
                 });
@@ -33,7 +35,7 @@ angular.module('mustard', [
                 var deferred = $q.defer();
                 $http({
                     method: "GET",
-                    url: "/mustard/www/js/game/scenarios/PracticeScenario.json"
+                    url: "js/game/scenarios/PracticeScenario.json"
                 }).success(function (response) {
                     deferred.resolve(response);
                 });
