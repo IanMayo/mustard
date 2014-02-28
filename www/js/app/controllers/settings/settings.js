d0c6784de2908b56f2ca6e99952cc1f967943521
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-02-28 17:22:00 +0200

    Create prototypical profile/settings controllers

---
+++ www/js/app/controllers/settings/settings.js
@@ -0,0 +1,9 @@
+angular.module('mustard.app.settings', [])
+
+/**
+ * @module Settings
+ * @class SettingsCtrl (controller)
+ */
+.controller('SettingsCtrl', function ($scope) {
+    // Do something
+});
---
+++ www/js/app/controllers/user-profile/profile.js
@@ -0,0 +1,9 @@
+angular.module('mustard.app.profile', [])
+
+/**
+ * @module Profile
+ * @class ProfileCtrl (controller)
+ */
+.controller('ProfileCtrl', function ($scope) {
+    // Do something
+});
