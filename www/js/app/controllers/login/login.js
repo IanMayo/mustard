f21e280c4f3021a6be85fe09e3f99594fcdd9b45
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-03-07 06:13:05 +0200

    Change login controller's logic to test the app with "mock" users

--- www/js/app/controllers/login/login.js
+++ www/js/app/controllers/login/login.js
@@ -1,9 +1,19 @@
-angular.module('mustard.app.login', [])
+angular.module('mustard.app.login', ['mustard.app.user'])
 
 /**
  * @module Login
  * @class LoginCtrl (controller)
  */
-.controller('LoginCtrl', function ($scope) {
-    // Do something
+.controller('LoginCtrl', function ($scope, $location, user) {
+
+    // TODO: Move to the bootstrap module
+    user.restoreFromLocal(function (isRestored) {
+        isRestored && $location.path('/main');
+    });
+
+    $scope.submit = function (username, password) {
+        user.restoreFromWeb(username, password).then(function (isRestored) {
+            isRestored && $location.path('/main');
+        });
+    };
 });
