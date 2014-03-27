a411e807a755ac5080f57fdfb3728d538bd5f6fa
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-03-27 22:49:35 +0200

    Experiments with index-missions task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,21 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
 
+        "index-missions": {
+            src: 'www/js/game/scenarios'
+        }
     });
 
-    grunt.registerTask('index-missions', []);
+    grunt.registerMultiTask('index-missions', 'Create missions index', function () {
+//        var scenarios = [];
+//
+//        console.log(this.filesSrc);
+//
+//        grunt.file.recurse('www/js/game/scenarios', function (abspath) {
+//            var file = grunt.file.readJSON(abspath);
+//            scenarios.push(file);
+//        });
+    });
 };
