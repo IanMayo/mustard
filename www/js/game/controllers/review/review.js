1bd3c676bc9b474be8439eda828caec4614754cd
Evgeny Demidchenko <promocoding@gmail.com>
2014-03-27 11:31:24 +0200

    Reformat code

--- www/js/game/controllers/review/review.js
+++ www/js/game/controllers/review/review.js
@@ -7,7 +7,7 @@ angular.module('mustard.game.review', [
     'mustard.game.objectiveListDirective',
     'mustard.game.reviewSnapshot',
     'mustard.game.geoMath'
-  ])
+])
 
 /**
  * @module Game
@@ -49,7 +49,7 @@ angular.module('mustard.game.review', [
      * @returns {String}
      */
     var vesselShortName = function (name) {
-      return name.replace(/\s+/g, '');
+        return name.replace(/\s+/g, '');
     };
 
     /**
@@ -60,11 +60,11 @@ angular.module('mustard.game.review', [
         var ownShipName = vesselShortName(_.keys($scope.history.vessels).shift());
 
         return {
-          name: function () {
+            name: function () {
               return ownShipName;
-          }
+            }
         }
-      };
+    };
 
     /**
      * Add necessary properties to create a map marker
@@ -73,86 +73,85 @@ angular.module('mustard.game.review', [
      * @returns {Object}
      */
     var markerOptions = function (vessel, name) {
-      var stateData = {
+        var stateData = {
         name: name,
-        state: {
-          course: 0,
-          location: {
-            lat: 0,
-            lng: 0
-          }
-        }
-      };
+            state: {
+            course: 0,
+                location: {
+                    lat: 0,
+                    lng: 0
+                }
+            }
+        };
 
-      return _.extend(vessel, stateData);
+        return _.extend(vessel, stateData);
     };
 
-      /**
-       * Update the UI to the current review time
-       */
-      var doUpdate = function () {
+    /**
+     * Update the UI to the current review time
+     */
+    var doUpdate = function () {
         // shortcut to the time
         var tNow = $scope.reviewState.reviewTime;
 
         // ok, retrieve the state, to update teh marker
         _.each($scope.history.vessels, function (vessel, name) {
 
-          // what's the time of the first element in this array
-          var firstTime = vessel.track[0].time;
+            // what's the time of the first element in this array
+            var firstTime = vessel.track[0].time;
 
-          // how much further along are we?
-          var delta = tNow - firstTime;
+            // how much further along are we?
+            var delta = tNow - firstTime;
 
-          // what's the index of the relevant array item
-          var index = delta / $scope.history.stepTime;
+            // what's the index of the relevant array item
+            var index = delta / $scope.history.stepTime;
 
             var nearest;
             var shortName;
             var thisV;
 
+            // is this less than the length?
+            if (index < vessel.track.length) {
+                nearest = vessel.track[index];
 
-          // is this less than the length?
-          if (index < vessel.track.length) {
-            nearest = vessel.track[index];
+                if (nearest) {
+                    shortName = vesselShortName(name);
+                    thisV = $scope.vessels[shortName];
 
-            if (nearest) {
-              shortName = vesselShortName(name);
-              thisV = $scope.vessels[shortName];
-
-              thisV.name = shortName
-              thisV.state = {
-                  speed: nearest.speed,
-                  location: {
-                      lat: nearest.lat,
-                      lng: nearest.lng
-                  },
-                  course: nearest.course
-              }
+                    thisV.name = shortName
+                    thisV.state = {
+                        speed: nearest.speed,
+                        location: {
+                            lat: nearest.lat,
+                            lng: nearest.lng
+                        },
+                        course: nearest.course
+                    }
+                }
+            } else {
+                // stop review process
+                $scope.reviewState.reviewTime = 0;
             }
-          } else {
-            // stop review process
-            $scope.reviewState.reviewTime = 0;
-          }
         });
 
         $scope.$broadcast('changeMarkers', $scope.vessels);
-      };
+    };
 
-      /**
-       * Show the vessel markers, plus their routes.
-       */
-      var showVesselRoutes = function () {
+    /**
+     * Show the vessel markers, plus their routes.
+     */
+    var showVesselRoutes = function () {
         // store the vessel routes
         var routes = [];
 
         // start out with the markes
         _.each($scope.history.vessels, function (vessel, name) {
 
-          // store this route
-          routes.push(vessel.track);
+            // store this route
+            routes.push(vessel.track);
 
-          // declare this marker
-          var shortName = vesselShortName(name);
+            // declare this marker
+            var shortName = vesselShortName(name);
             $scope.vessels[shortName] = markerOptions(vessel, shortName);
         });
 
@@ -161,14 +160,14 @@ angular.module('mustard.game.review', [
             $scope.$broadcast('vesselRoutes', routes);
             $scope.$broadcast('changeTargetsVisibility', true);
         }, 100);
-      };
+    };
 
-      var showNarrativeMarkers = function () {
+    var showNarrativeMarkers = function () {
         // loop through the narratives
-          var narratives = {};
-        _.each($scope.history.narratives, function (item, index) {
+        var narratives = {};
 
-          var narrMessage = "Time:" + item.time + "<br/>" + item.message;
+        _.each($scope.history.narratives, function (item, index) {
+            var narrMessage = "Time:" + item.time + "<br/>" + item.message;
             narratives['narrative_' + index] = {
                 location: {
                     lat: item.location.lat,
@@ -181,40 +180,39 @@ angular.module('mustard.game.review', [
         });
 
         $timeout(function () {
-          $scope.$broadcast('narrativeMarkers', narratives);
+            $scope.$broadcast('narrativeMarkers', narratives);
         }, 100);
-      };
+    };
 
-      // create a wrapped ownship instance, for convenience
-      $scope.ownShip = ownShipApi();
+    // create a wrapped ownship instance, for convenience
+    $scope.ownShip = ownShipApi();
 
-      $scope.$watch('reviewState.reviewTime', function (newVal) {
-          if (newVal) {
-              doUpdate();
-          }
-      });
+    $scope.$watch('reviewState.reviewTime', function (newVal) {
+        if (newVal) {
+            doUpdate();
+        }
+    });
 
-      /**
-       * Provide back button support
-       */
-      $scope.goBack = function () {
+    /**
+     * Provide back button support
+     */
+    $scope.goBack = function () {
         window.history.back();
-      };
+    };
 
-      // show the markers, plus their routes
-      showVesselRoutes();
+    // show the markers, plus their routes
+    showVesselRoutes();
 
-      // show markers for the narrative entires
-      showNarrativeMarkers();
+    // show markers for the narrative entires
+    showNarrativeMarkers();
 
-      // put the markers on the map in their initial locations
-      doUpdate();
+    // put the markers on the map in their initial locations
+    doUpdate();
 
-      // Note: the narrative "tour" should not require a button press to start, it should just run.
-      // but, we may provide UI control to restart tour.
-      $scope.showNarrative = function () {
+    // Note: the narrative "tour" should not require a button press to start, it should just run.
+    // but, we may provide UI control to restart tour.
+    $scope.showNarrative = function () {
 
-        // start the tour
-      }
+    // start the tour
     }
-]);
+}]);
