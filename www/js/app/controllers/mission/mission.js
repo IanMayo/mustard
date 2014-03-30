e2b0ae80d665e833f5229078f8f3fa2217fbe704
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-03-30 19:11:48 +0300

    Simplify mission controller after adding initial mission data

--- www/js/app/controllers/mission/mission.js
+++ www/js/app/controllers/mission/mission.js
@@ -4,24 +4,13 @@ angular.module('mustard.app.mission', [])
  * @module Mission
  * @class MissionCtrl (controller)
  */
-.controller('MissionCtrl', ['$scope', '$routeParams', '$location', 'reviewSnapshot',
-    function ($scope, $routeParams, $location, reviewSnapshot) {
+.controller('MissionCtrl', ['$scope', '$routeParams', '$location', 'reviewSnapshot', 'mission',
+    function ($scope, $routeParams, $location, reviewSnapshot, mission) {
 
-    /** make the id available to the scope
-     *
-     */
-    $scope.missionNumber = $routeParams.id;
+    $scope.mission = mission;
 
-    /* TODO: So let's keep things simple for now and leave this url creating and potential 404 problem here.
-     * for managing potential 404 error we could use something like this
-     * https://github.com/matys84pl/angularjs-nginclude-handling-404/blob/master/www/js/app/main.js
-     * or this
-     * http://stackoverflow.com/questions/20836374/how-to-catch-angular-ng-include-error
-     */
-    $scope.guidanceUrl = 'guidance/' + $routeParams.id + '/index.html';
-
-    $scope.moveToMission = function (id) {
-        $location.path('/game/mission/' + id);
+    $scope.moveToMission = function (missionUrl) {
+        $location.path('/game/mission/' + missionUrl);
     };
 
     $scope.doReview = function () {
