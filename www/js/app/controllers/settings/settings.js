a0dc419ea8738008830999f4d08fbf8ecee4ca98
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-04-03 04:34:01 +0300

    Add prototypical volume settings

--- www/js/app/controllers/settings/settings.js
+++ www/js/app/controllers/settings/settings.js
@@ -1,9 +1,15 @@
-angular.module('mustard.app.settings', [])
+angular.module('mustard.app.settings', [
+    'mustard.game.audio'
+])
 
 /**
  * @module Settings
  * @class SettingsCtrl (controller)
  */
-.controller('SettingsCtrl', function ($scope) {
-    // Do something
+.controller('SettingsCtrl', function ($scope, audio) {
+    $scope.volume = audio.volume();
+
+    $scope.$watch('volume', function (newValue) {
+        audio.volume(newValue);
+    });
 });
