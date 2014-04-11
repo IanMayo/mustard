f9f2a7616d11acc0441469b7644006d0a6c4938b
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-04-11 19:37:02 +0300

    Adds showModal method to the message service

--- www/js/game/services/message/message.js
+++ www/js/game/services/message/message.js
@@ -4,9 +4,36 @@
 
 angular.module('mustard.app.message', [])
 
-.factory('user', function () {
+.factory('message', function ($modal) {
+
+    var ModalInstanceController = function ($scope, $modalInstance, messageData) {
+        $scope.messageData = messageData;
+
+        $scope.ok = function () {
+            $modalInstance.dismiss('ok');
+        };
+    };
+
+    var modalOptions = {
+        templateUrl: 'js/game/services/message/message.html',
+        controller: ModalInstanceController
+    };
 
     return {
+        showModal: function (type, title, text) {
+            var messageData = {
+                type: type,
+                title: title,
+                text: text
+            };
 
+            return $modal.open(angular.extend(modalOptions, {
+                resolve: {
+                    messageData: function () {
+                        return messageData;
+                    }
+                }
+            }));
+        }
     };
 });
