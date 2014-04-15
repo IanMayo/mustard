bc14d8ad6556ec694f4d74380f0d66d50fb00523
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-04-15 23:49:09 +0300

    Add click-repeat directive prototype

---
+++ www/js/game/directives/clickRepeat/clickRepeat.js
@@ -0,0 +1,15 @@
+angular.module('mustard.game.clickRepeat', [])
+
+.directive('clickRepeat', ['$interval', function ($interval) {
+
+    return {
+        restrict: 'A',
+        scope: {
+
+        },
+
+        link: function (scope) {
+
+        }
+    };
+}]);
