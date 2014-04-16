cfcba2a1c691175a19adf76e5b0b4a26dc6d9ef5
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-04-16 03:55:40 +0300

    Rebuild android/iOS apps

---
+++ platforms/ios/www/js/game/directives/reviewTour/reviewTour.js
@@ -0,0 +1,76 @@
+/**
+ * @module mustard.game.reviewTourDirective
+ */
+
+angular.module('mustard.game.reviewTourDirective', ['mustard.game.leafletMapDirective'])
+
+.directive('reviewTour', ['$timeout', function ($timeout) {
+    return {
+        restrict: 'EA',
+        controller: ['$scope', function ($scope) {
+            var narrativeSteps = [];
+            var currentStep = 0;
+            var tour;
+            var $stepWindow;
+
+            var changeTime = function () {
+                var step = tour.getCurrentStep();
+                $timeout(function () {
+                    $scope.reviewState.reviewTime = narrativeSteps[step].time;
+                });
+            };
+
+            tour = new Tour({
+                onShown: changeTime,
+                storage: false,
+                animation: true,
+                template: '<div class="popover tour">' +
+                    '<div class="arrow"></div>' +
+                    '<div class="popover-title"></div>' +
+                    '<div class="popover-content"></div>' +
+                    '<div class="popover-navigation">' +
+                        '<div class="btn-group">' +
+                            '<button class="btn btn-sm btn-default" data-role="prev"><span class="glyphicon glyphicon-backward"></span></button>' +
+                            '<button class="btn btn-sm btn-default" data-role="next"><span class="glyphicon glyphicon-forward"></span></button>' +
+                        '</div>' +
+                        '<button class="btn btn-sm btn-default" data-role="end"><span class="glyphicon glyphicon-stop"></span></button>' +
+                    '</div>' +
+                '</div>'
+            });
+
+            tour.init();
+
+            $scope.showNarrative = function () {
+                tour.end();
+                tour.restart();
+            };
+
+
+            $scope.$on("$routeChangeStart", function () {
+                tour.end();
+            });
+
+            return {
+                setNarrativeSteps: function (steps) {
+                    narrativeSteps = steps;
+                    tour.addSteps(steps);
+                    tour.start();
+                },
+                hideSteps: function () {
+                    currentStep = tour.getCurrentStep();
+                    $stepWindow = $('#step-' + currentStep);
+                    $stepWindow.hide();
+                },
+                showSteps: function () {
+                    tour.showStep(currentStep);
+                    $timeout(function () {
+                        // drag the map changes position of a narrative marker
+                        // wait while method showStep() applies new position according to new coordinates of the marker
+                        $stepWindow.show();
+                    }, 1000);
+                }
+            };
+        }]
+    }
+}]);
+
