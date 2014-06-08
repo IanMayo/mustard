10e5658596dc29fd9e2156753f31e86cf712a558
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-06-08 19:50:59 +0300

    Rename settings module to options module

---
+++ www/js/app/controllers/options/options.js
@@ -0,0 +1,9 @@
+angular.module('mustard.app.options', ['mustard.app.user'])
+
+/**
+ * @module Options
+ * @class OptionsCtrl (controller)
+ */
+.controller('OptionsCtrl', function ($scope, user) {
+
+});
