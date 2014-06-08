bbae2b638ea99581c26edec5ed61563c686ff826
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-06-08 23:09:01 +0300

    Add controls to the options page and make them work

--- www/js/app/controllers/options/options.js
+++ www/js/app/controllers/options/options.js
@@ -6,4 +6,19 @@ angular.module('mustard.app.options', ['mustard.app.user'])
  */
 .controller('OptionsCtrl', function ($scope, user) {
 
+    /**
+     * Local options model
+     */
+    $scope.options = angular.extend({
+        audio: 0,
+        sfx: 0,
+        language: "EN"
+    }, user.options);
+
+    /**
+     * Watches on the local options model and changes the user's one
+     */
+    $scope.$watchCollection('options', function (value) {
+        user.setOptions(value);
+    });
 });
