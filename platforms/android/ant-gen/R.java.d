373c215cd4b0068c52b5cc67efd7212eb58ca7aa
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-06-15 18:16:27 +0300

    Rebuild android/iOS apps

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -6,4 +6,9 @@
 /Users/alexander/Repos/mustard/platforms/android/res/drawable-xhdpi/icon.png \
 /Users/alexander/Repos/mustard/platforms/android/res/values/strings.xml \
 /Users/alexander/Repos/mustard/platforms/android/res/xml/config.xml \
+/Users/alexander/Repos/mustard/platforms/android/ant-build/res/drawable/icon.png \
+/Users/alexander/Repos/mustard/platforms/android/ant-build/res/drawable-hdpi/icon.png \
+/Users/alexander/Repos/mustard/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/Users/alexander/Repos/mustard/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/Users/alexander/Repos/mustard/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
 /Users/alexander/Repos/mustard/platforms/android/ant-build/AndroidManifest.xml \
