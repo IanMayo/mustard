d0cd2b2f02a849ac5fbba32130b06209badce593
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-06-15 17:59:49 +0300

    Rebuild and update android/iOS apps (phonegap 3.5.0)

---
+++ platforms/android/ant-gen/R.java.d
@@ -0,0 +1,9 @@
+/Users/alexander/Repos/mustard/platforms/android/ant-gen/com/phonegap/mustard/R.java \
+ : /Users/alexander/Repos/mustard/platforms/android/res/drawable/icon.png \
+/Users/alexander/Repos/mustard/platforms/android/res/drawable-hdpi/icon.png \
+/Users/alexander/Repos/mustard/platforms/android/res/drawable-ldpi/icon.png \
+/Users/alexander/Repos/mustard/platforms/android/res/drawable-mdpi/icon.png \
+/Users/alexander/Repos/mustard/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/alexander/Repos/mustard/platforms/android/res/values/strings.xml \
+/Users/alexander/Repos/mustard/platforms/android/res/xml/config.xml \
+/Users/alexander/Repos/mustard/platforms/android/ant-build/AndroidManifest.xml \
