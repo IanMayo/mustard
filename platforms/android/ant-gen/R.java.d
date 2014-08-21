2bafbaee4281b7cba81a9a84ff2ebc9f136539c8
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-08-21 03:56:19 +0300

    Recompile android app

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -1,17 +1,13 @@
 /Users/alexander/Repos/mustard/platforms/android/ant-gen/com/subtrack90/R.java \
  : /Users/alexander/Repos/mustard/platforms/android/res/drawable/icon.png \
 /Users/alexander/Repos/mustard/platforms/android/res/drawable-hdpi/icon.png \
-/Users/alexander/Repos/mustard/platforms/android/res/drawable-land-hdpi/screen.png \
-/Users/alexander/Repos/mustard/platforms/android/res/drawable-land-ldpi/screen.png \
-/Users/alexander/Repos/mustard/platforms/android/res/drawable-land-mdpi/screen.png \
-/Users/alexander/Repos/mustard/platforms/android/res/drawable-land-xhdpi/screen.png \
+/Users/alexander/Repos/mustard/platforms/android/res/drawable-hdpi/screen.png \
 /Users/alexander/Repos/mustard/platforms/android/res/drawable-ldpi/icon.png \
+/Users/alexander/Repos/mustard/platforms/android/res/drawable-ldpi/screen.png \
 /Users/alexander/Repos/mustard/platforms/android/res/drawable-mdpi/icon.png \
-/Users/alexander/Repos/mustard/platforms/android/res/drawable-port-hdpi/screen.png \
-/Users/alexander/Repos/mustard/platforms/android/res/drawable-port-ldpi/screen.png \
-/Users/alexander/Repos/mustard/platforms/android/res/drawable-port-mdpi/screen.png \
-/Users/alexander/Repos/mustard/platforms/android/res/drawable-port-xhdpi/screen.png \
+/Users/alexander/Repos/mustard/platforms/android/res/drawable-mdpi/screen.png \
 /Users/alexander/Repos/mustard/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/alexander/Repos/mustard/platforms/android/res/drawable-xhdpi/screen.png \
 /Users/alexander/Repos/mustard/platforms/android/res/values/strings.xml \
 /Users/alexander/Repos/mustard/platforms/android/res/xml/config.xml \
 /Users/alexander/Repos/mustard/platforms/android/ant-build/AndroidManifest.xml \
