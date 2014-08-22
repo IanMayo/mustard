b3419864e8f02f18fb5f14f3371f37564d256113
Ian Mayo <ian@planetmayo.com>
2014-08-22 14:30:18 +0100

    On Mission page, populate the Situation Block.  Also, show the list of actual objectives for that mission.
    We've had to change the missionsIndex Grunt task to support this.

--- tasks/missionsIndex.js
+++ tasks/missionsIndex.js
@@ -141,10 +141,45 @@ module.exports = function(grunt) {
 
                 var fileName = getFileNameWithoutExtension(mission._url_);
 
+                // produce a trimmed down list of mission objectives, just containing the names and descriptions
+                var objectives = [];
+                mission.objectives.forEach(function(objective)
+                {
+                    // declare this objective
+                    var thisO = {id: objective.id, name: objective.name, description: objective.description };
+
+                    // also sort out any children
+                    if(objective.children)
+                    {
+                        var childList = [];
+
+                        // loop through them
+                        objective.children.forEach(function (childObj) {
+                                childList[childList.length] = {
+                                    name: childObj.name,
+                                    description: childObj.description,
+                                    type: childObj.type
+                                };
+                            }
+                        );
+                    }
+
+                    // did we find any?
+                    if(childList) {
+                        thisO.children =  childList;
+                    }
+
+                    // and store this new objective
+                    objectives[objectives.length] = thisO;
+
+                })
+
                 missionsIndex[rootIndex].missions.push({
                     id: mission.id,
                     name: mission.name,
                     description: mission.description,
+                    situation: mission.situation,
+                    objectives: objectives,
                     url: fileName,
                     guidanceUrl: guidanceDir + fileName + '.html'
                 });
