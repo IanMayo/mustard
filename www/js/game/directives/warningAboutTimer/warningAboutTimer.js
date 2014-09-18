7dba80de70b8de3e01fb22f71eae82b5cbdf4f40
Evgeny Demidchenko <promocoding@gmail.com>
2014-09-18 15:39:37 +0300

    Add simulationTimeStep to the math. Added debug information

--- www/js/game/directives/warningAboutTimer/warningAboutTimer.js
+++ www/js/game/directives/warningAboutTimer/warningAboutTimer.js
@@ -4,30 +4,47 @@
 
 angular.module('subtrack90.game.warningAboutTimer', ['subtrack90.game.timeDisplayDirective'])
 
-.directive('soundAtSecond', ['$sce', 'timeAccelerated',
-    function ($sce, timeAccelerated) {
+.directive('soundAtSecond', ['$sce', 'timeAccelerated', '$interval',
+    function ($sce, timeAccelerated, $interval) {
     return {
         restrict: 'EA',
         replace: true,
         scope: {
             time: '=',
+            timeStep: '@',
             second: '@',
             src: '@'
         },
-        template: "<audio><source src=\"{{getAudioUrl()}}\" type=\"audio/mpeg\">" +
+        template: "<div><audio><source src=\"{{getAudioUrl()}}\" type=\"audio/mpeg\">" +
             "Your browser does not support the audio tag. " +
-            "</audio>",
+            "</audio><span class=\"label label-warning\" style='margin-left: 10px'>{{countdownLabel}}</span></div>",
         link: function (scope, element) {
             scope.getAudioUrl = function () {
                 return $sce.trustAsResourceUrl('audio/' + scope.src);
             };
 
+            scope.countdownLabel = 'Wait alarm sound...';
+
             var clearWatchingTime = scope.$watch('time', function (millisec) {
-                if (millisec / timeAccelerated.current() < parseInt(scope.second) * 1000) {
+                if (millisec / timeAccelerated.current() < parseInt(scope.second) * parseInt(scope.timeStep)) {
                     clearWatchingTime();
-                    element[0].play();
+                    element.children()[0].play();
+                    countdown();
                 }
             });
+
+            var countdown = function () {
+                var startValue = parseInt(scope.second);
+                scope.countdownLabel = startValue;
+
+                var interval = $interval(function () {
+                    startValue = startValue - 1;
+                    scope.countdownLabel = startValue;
+                    if (!scope.countdownLabel) {
+                        $interval.cancel(interval);
+                    }
+                }, 1000);
+            }
         }
     }
 }])
@@ -37,11 +54,12 @@ angular.module('subtrack90.game.warningAboutTimer', ['subtrack90.game.timeDispla
         restrict: 'EA',
         scope: {
             time: '=',
+            timeStep: '@',
             second: '@'
         },
         link: function (scope, element) {
             var clearWatchingTime = scope.$watch('time', function (millisec) {
-                if (millisec / timeAccelerated.current() < parseInt(scope.second) * 1000) {
+                if (millisec / timeAccelerated.current() < parseInt(scope.second) * parseInt(scope.timeStep)) {
                     clearWatchingTime();
                     element.addClass('play-warning');
                 }
