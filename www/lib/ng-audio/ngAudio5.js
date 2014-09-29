bd37672fda444f77156f09bf820edb3bd4adcc74
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-09-29 21:08:42 +0300

    Add audio5 sound library to the project

---
+++ www/lib/ng-audio/ngAudio5.js
@@ -0,0 +1,13 @@
+angular.module('Audio5', []).factory('AudioService', function () {
+    "use strict";
+
+    var params = {
+        swf_path:'../../swf/audio5js.swf',
+        throw_errors:true,
+        format_time:true
+    };
+
+    var audio5js = new Audio5js(params);
+
+    return audio5js;
+});
