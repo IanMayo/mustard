8274898d1f924cfb274d0a17443655431fd801d4
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-09-30 05:34:48 +0300

    Remove audio5.js from the project

--- www/js/app/services/sound/sound.js
+++ www/js/app/services/sound/sound.js
@@ -4,11 +4,11 @@
  * @description This is the wrapper service for html5audio & native mobile audio libs
  */
 
-angular.module('subtrack90.app.sound', ['ngCordova', 'Audio5'])
+angular.module('subtrack90.app.sound', ['ngCordova'])
 
-.factory('sound', function (IS_CORDOVA, $cordovaNativeAudio, AudioService) {
+.factory('sound', function (IS_CORDOVA, $cordovaNativeAudio) {
 
-    return {
+    var html5Audio = {
         play: function (path) {
 
         },
@@ -20,5 +20,21 @@ angular.module('subtrack90.app.sound', ['ngCordova', 'Audio5'])
         volume: function (value) {
 
         }
-    }
+    };
+
+    var mobileAudio = {
+        play: function (path) {
+
+        },
+
+        loop: function (path) {
+
+        },
+
+        volume: function (value) {
+
+        }
+    };
+
+    return IS_CORDOVA ? mobileAudio : html5Audio
 });
