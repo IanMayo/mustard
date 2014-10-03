59f610406dc41af718df5fff0959f4d5a64d835c
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-10-03 03:57:08 +0300

    Let's add some Howler proto just to test the possibility to play the sound(s)

--- www/js/app/services/sound/sound.js
+++ www/js/app/services/sound/sound.js
@@ -8,20 +8,39 @@ angular.module('subtrack90.app.sound', ['ngCordova'])
 
 .factory('sound', function (IS_CORDOVA, $cordovaNativeAudio) {
 
+    /**
+     * Audio wrapper for howler.js lib
+     *
+     * @type {Object}
+     */
     var html5Audio = {
-        play: function (path) {
-
+        play: function (path, volume) {
+            new Howl({
+                urls: [path],
+                volume: volume
+            }).play();
         },
 
-        loop: function (path) {
-
+        loop: function (path, volume) {
+            new Howl({
+                urls: [path],
+                loop: true,
+                volume: volume
+            }).play();
         },
 
         volume: function (value) {
-
+            Howler.volume(value);
         }
     };
 
+    /**
+     * Audio wrapper for native mobile audio plugin which is based on
+     * https://github.com/SidneyS/cordova-plugin-nativeaudio
+     * http://ngcordova.com/docs/#NativeAudio
+     *
+     * @type {Object}
+     */
     var mobileAudio = {
         play: function (path) {
 
