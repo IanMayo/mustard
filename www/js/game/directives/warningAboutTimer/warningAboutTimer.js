590f20393d6f0b01bd93cd969070223dbeece304
Evgeny Demidchenko <promocoding@gmail.com>
2014-10-09 09:27:58 +0300

    Add WarningAlarm directive. Refactor SoundAtSecond and WarningBackground directives

--- www/js/game/directives/warningAboutTimer/warningAboutTimer.js
+++ www/js/game/directives/warningAboutTimer/warningAboutTimer.js
@@ -3,68 +3,86 @@
  */
 
 angular.module('subtrack90.game.warningAboutTimer', ['subtrack90.game.timeDisplayDirective'])
-
-.directive('soundAtSecond', ['$sce', 'timeAccelerated',
-    function ($sce, timeAccelerated) {
+    
+.directive('warningAlarm', ['$sce', 'timeAccelerated', function ($sce, timeAccelerated) {
     return {
         restrict: 'EA',
         replace: true,
         scope: {
             time: '=',
             timeStep: '&',
-            play: '&'
+            warn: '&'
         },
-        template: "<audio><source src=\"{{getAudioUrl()}}\" type=\"audio/mpeg\">" +
-            "Your browser does not support the audio tag. " +
-            "</audio>",
-        link: function (scope, element) {
+        controller: function ($scope) {
             var warningIsActive = false;
-
-            scope.getAudioUrl = function () {
-                return $sce.trustAsResourceUrl('audio/' + scope.play().track);
+            var tasks = [];
+            var runTask = function (type) {
+                _.each(tasks, function (task) {
+                    if (angular.isFunction(task[type])) {
+                        task[type]();
+                    }
+                })
             };
 
-            scope.$watch('time', function (millisec, oldVal) {
+            $scope.$watch('time', function (millisec, oldVal) {
                 if (oldVal > millisec) {
-                    // play warning sound for a new time-out
+                    // show warning background for a new time-out
                     if (false === warningIsActive &&
-                        millisec / timeAccelerated.current() < parseInt(scope.play().inSecond) * scope.timeStep()) {
-                        // play warning sound once for the time-out
-                        element[0].play();
+                        millisec / timeAccelerated.current() < parseInt($scope.warn().inSecond) * $scope.timeStep()) {
+                        // show warning only once for the time-out
+                        runTask('warn');
                         warningIsActive = true;
                     }
                 } else {
                     // next time-out alarm was added
+                    runTask('removeWarn');
                     warningIsActive = false;
+                }
+            });
+
+            this.getAudioUrl = function () {
+                return $sce.trustAsResourceUrl('audio/' + $scope.warn().sound);
+            };
+
+            this.addTask = function (task) {
+                tasks.push(task);
+            };
+        }
+    }
+}])
+    
+.directive('soundAtSecond', function () {
+    return {
+        restrict: 'EA',
+        replace: true,
+        require: 'warningAlarm',
+        template: "<audio><source src=\"{{getAudioUrl()}}\" type=\"audio/mpeg\">" +
+            "Your browser does not support the audio tag. " +
+            "</audio>",
+        link: function (scope, element, attr, controller) {
+            scope.getAudioUrl = function () {
+                return controller.getAudioUrl();
+            };
+
+            controller.addTask({
+                warn: function () {
+                    element[0].play();
                 }
             });
         }
     }
-}])
+})
 
-.directive('warningBackground', ['timeAccelerated', function (timeAccelerated) {
+.directive('warningBackground', function () {
     return {
         restrict: 'EA',
-        scope: {
-            time: '=',
-            timeStep: '&',
-            warn: '&'
-        },
-        link: function (scope, element) {
-            var warningIsActive = false;
-
-            scope.$watch('time', function (millisec, oldVal) {
-                if (oldVal > millisec) {
-                    // show warning background for a new time-out
-                    if (false === warningIsActive &&
-                        millisec / timeAccelerated.current() < parseInt(scope.warn().inSecond) * scope.timeStep()) {
-                        // show warning only once for the time-out
-                        element.addClass('play-warning');
-                        warningIsActive = true;
-                    }
-                } else {
-                    // next time-out alarm was added
-                    warningIsActive = false;
+        require: 'warningAlarm',
+        link: function (scope, element, attr, controller) {
+            controller.addTask({
+                warn: function () {
+                    element.addClass('play-warning');
+                },
+                removeWarn: function () {
                     if (element.hasClass('play-warning')) {
                         // Remove warning background what was shown for previous time-out
                         element.removeClass('play-warning');
@@ -73,4 +91,4 @@ angular.module('subtrack90.game.warningAboutTimer', ['subtrack90.game.timeDispla
             });
         }
     }
-}]);
+});
