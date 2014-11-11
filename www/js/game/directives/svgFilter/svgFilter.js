4b63436d0adf3f4d6f08335712d2efe30a4ad400
Evgeny Demidchenko <promocoding@gmail.com>
2014-11-11 16:32:12 +0200

    Add demo filters to test them on fly

--- www/js/game/directives/svgFilter/svgFilter.js
+++ www/js/game/directives/svgFilter/svgFilter.js
@@ -11,6 +11,7 @@ angular.module('subtrack90.game.svgFilter', [])
 .directive('svgFilter', function () {
     return {
         restrict: 'EA',
+        template: '<select ng-model="svgFilter" ng-options="filter as filter.name for filter in filters"></select>',
         link: function (scope, el) {
             var blurFilterName = 'blurElement';
             var svgContainer = d3.select(el[0])
@@ -23,53 +24,113 @@ angular.module('subtrack90.game.svgFilter', [])
             var filter = defs.append('filter')
                 .attr('id', blurFilterName);
 
-            var feConvolve = filter.append('feConvolveMatrix')
-                .attr('result', 'out1');
-
-            var filterType = 5;
-            var matrix;
-            var order;
-            var blendMode;
-
-            switch (filterType) {
-                case 1:
-                    order = '3';
-                    matrix = '1 1 1 1 1 1 1 1 1';
-                    break;
-                case 2:
-                    order = '3';
-                    matrix = '1 0 0 0 0 0 0 0 1';
-                    break;
-                case 3:
-                    order = '10,1';
-                    matrix = '1 1 1 1 1 1 1 1 1 1';
-                    break;
-                case 4:
-                    order = '1,10';
-                    matrix = '1 1 1 1 1 1 1 1 1 1';
-                    break;
-                case 5:
-                    order = '10,1';
-                    matrix = '1 1 1 1 1 1 1 1 1 1';
-                    blendMode = 'screen';
-                    break;
-                case 6:
-                    order = '10,1';
-                    matrix = '0 0 0 0 0 1 1 1 1 1';
-                    blendMode = 'screen';
-                    break;
+            function blurFilter() {
+                removeFilter();
+                var feGaussianBlur = filter.append('feGaussianBlur')
+                    .attr({'in': 'SourceGraphic', 'stdDeviation': '2' })
             }
 
-            feConvolve
-                .attr('kernelMatrix', matrix)
-                .attr('order', order);
+            function blendBlurFilter() {
+                removeFilter();
 
-            if (blendMode) {
+                filter.append('feOffset')
+                    .attr({
+                        'in': 'SourceGraphic',
+                        'result': 'offset',
+                        'dx': '2',
+                        'dy': '2'
+                    });
+
+                var feGaussianBlur = filter.append('feGaussianBlur')
+                    .attr({'in': 'SourceGraphic', 'stdDeviation': '2' })
+                    .attr('result', 'blur');
+
+                filter.append('feBlend')
+                    .attr('in', 'blur')
+                    .attr('in2', 'SourceGraphic')
+                    .attr('mode', 'screen');
+            }
+
+            function compositeFilter() {
+                removeFilter();
+                var feGaussianBlur = filter.append('feGaussianBlur')
+                    .attr({'in': 'SourceGraphic', 'stdDeviation': '2' })
+                    .attr('result', 'blur');
+
+                filter.append('feComposite')
+                    .attr({
+                        'in2': 'blur',
+                        'in': 'SourceGraphic',
+                        'operator': 'arithmetic',
+                        'k1': 0,
+                        'k2': .5,
+                        'k3': .6,
+                        'k4': 0
+                    });
+            }
+
+            function convolveFilter() {
+                removeFilter();
+                filter.append('feConvolveMatrix')
+                        .attr('result', 'convolve')
+                        .attr('order', '10,1')
+                        .attr('kernelMatrix', '1 1 1 1 1 1 1 1 1 1');
+
                 filter.append('feBlend')
                     .attr('in', 'SourceGraphic')
-                    .attr('in2', 'out1')
-                    .attr('mode', blendMode);
+                    .attr('in2', 'convolve')
+                    .attr('mode', 'screen');
             }
+
+            function convolveNoiseFilter() {
+                removeFilter();
+                filter.append('feConvolveMatrix')
+                        .attr('result', 'convolve')
+                        .attr('order', '10,1')
+                        .attr('kernelMatrix', '1 1 0 0 0 0 1 1 1 1');
+
+                filter.append('feBlend')
+                    .attr('in', 'SourceGraphic')
+                    .attr('in2', 'convolve')
+                    .attr('mode', 'screen');
+            }
+
+            function filterList() {
+                scope.filters = [
+                    {
+                        name: 'blur',
+                        handler: blurFilter
+                    },
+                    {
+                        name: 'blur + blend',
+                        handler: blendBlurFilter
+                    },
+                    {
+                        name: 'composite',
+                        handler: compositeFilter
+                    },
+                    {
+                        name: 'convolve',
+                        handler: convolveFilter
+                    },
+                    {
+                        name: 'convolve + noise',
+                        handler: convolveNoiseFilter
+                    }
+                ];
+                scope.svgFilter = scope.filters[0];
+                el.css({display: 'block', position: 'absolute', right: '20px', top: '30px', zIndex: 1000});
+
+                scope.$watch('svgFilter', function (selectedFilter) {
+                    selectedFilter.handler.call();
+                });
+            }
+
+            function removeFilter() {
+                $(filter[0][0]).empty();
+            }
+
+            filterList();
         }
     }
 });
