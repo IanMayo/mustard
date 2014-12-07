c052d204deace4e4a09a74802eae30e37ceefc51
Alexander Vasilenko <vasilenko_alex@odesk.com>
2014-12-07 23:23:02 +0200

    Add Full Screen Control directive

---
+++ www/js/app/directives/fullScreenControl/fullScreenControl.js
@@ -0,0 +1,26 @@
+/**
+ * @module subtrack90.game.fullScreenControl
+ */
+
+angular.module('subtrack90.app.fullScreenControl', ['subtrack90.app.fullScreen'])
+
+/**
+ * Full screen control directive
+ */
+
+.directive('fullScreenControl', function (IS_MOBILE, IS_CORDOVA, fullScreen) {
+
+    return {
+        restrict: 'A',
+        replace: true,
+        templateUrl: 'js/app/directives/fullScreenControl/fullScreenControl.tpl.html',
+
+        link: function (scope) {
+            scope.hasShown = !IS_MOBILE && !IS_CORDOVA;
+
+            scope.toggleFullScreenMode = function () {
+                fullScreen.toggle();
+            };
+        }
+    };
+});
